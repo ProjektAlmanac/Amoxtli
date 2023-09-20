42628d387ea2e572e1062c5c20f1944ad21e685d
Antar Espadas <antar.espadas@hotmail.com>
2023-09-19 22:53:45 -0600

    Añade configuración de seguridad

---
+++ backend/src/main/java/io/github/projektalmanac/amoxtli/backend/config/SecurityConfig.java
@@ -0,0 +1,14 @@
+package io.github.projektalmanac.amoxtli.backend.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(org.springframework.security.config.annotation.web.builders.HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/**").permitAll();
+    }
+}
