b88af3382548b65564b32331cf3e32a9259fec36
Gisellrom <romero.giselln@gmail.com>
2023-09-29 03:21:50 -0600

    recupera informacion del usuario

--- frontend/src/app/features/perfil/pages/perfil.component.spec.ts
+++ frontend/src/app/features/perfil/pages/perfil.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { PerfilComponent } from './perfil.component'
+import { RouterTestingModule } from '@angular/router/testing'
+import { NoopAnimationsModule } from '@angular/platform-browser/animations'
+import { HttpClientTestingModule } from '@angular/common/http/testing'
 
 describe('PerfilComponent', () => {
   let component: PerfilComponent
@@ -9,6 +12,7 @@ describe('PerfilComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [PerfilComponent],
+      imports: [NoopAnimationsModule, RouterTestingModule, HttpClientTestingModule],
     })
     fixture = TestBed.createComponent(PerfilComponent)
     component = fixture.componentInstance
