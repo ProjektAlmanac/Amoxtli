e9aa38aaaddad0d030309f067f45d7d6f82428bf
Miguel Guzman <miguel.gm98@gmail.com>
2023-09-30 23:35:28 -0600

    Corregir archivos spec

--- frontend/src/app/shared/components/progress-spinner/progress-spinner.component.spec.ts
+++ frontend/src/app/shared/components/progress-spinner/progress-spinner.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { ProgressSpinnerComponent } from './progress-spinner.component'
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
 
 describe('ProgressSpinnerComponent', () => {
   let component: ProgressSpinnerComponent
@@ -9,6 +10,7 @@ describe('ProgressSpinnerComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [ProgressSpinnerComponent],
+      imports: [MatProgressSpinnerModule],
     })
     fixture = TestBed.createComponent(ProgressSpinnerComponent)
     component = fixture.componentInstance
