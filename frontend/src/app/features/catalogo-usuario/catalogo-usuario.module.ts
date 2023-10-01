dd95f8f0090bee216a579743447f6d1e43c325d4
Miguel Guzman <miguel.gm98@gmail.com>
2023-10-01 13:37:33 -0600

    Sort lineas

--- frontend/src/app/features/catalogo-usuario/catalogo-usuario.module.ts
+++ frontend/src/app/features/catalogo-usuario/catalogo-usuario.module.ts
@@ -1,7 +1,17 @@
-import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
-
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { NgIf } from '@angular/common'
+import { NgModule } from '@angular/core'
+import { RouterModule } from '@angular/router'
+
+import { SharedModule } from 'src/app/shared/shared.module'
+
+import { AgregarLibroComponent } from './components/agregar-libro/agregar-libro.component'
+import { HomePageComponent } from './pages/home-page/home-page.component'
+import { MiCatalogoPageComponent } from './pages/mi-catalogo-page/mi-catalogo-page.component'
+
+import { TextFieldModule } from '@angular/cdk/text-field'
+
 import { MatButtonModule } from '@angular/material/button'
 import { MatCardModule } from '@angular/material/card'
 import { MatFormFieldModule } from '@angular/material/form-field'
@@ -10,14 +20,6 @@ import { MatInputModule } from '@angular/material/input'
 import { MatListModule } from '@angular/material/list'
 import { MatSidenavModule } from '@angular/material/sidenav'
 import { MatToolbarModule } from '@angular/material/toolbar'
-import { NgIf } from '@angular/common'
-import { RouterModule } from '@angular/router'
-
-import { AgregarLibroComponent } from './components/agregar-libro/agregar-libro.component'
-import { HomePageComponent } from './pages/home-page/home-page.component'
-import { MiCatalogoPageComponent } from './pages/mi-catalogo-page/mi-catalogo-page.component'
-import { SharedModule } from 'src/app/shared/shared.module'
-import { TextFieldModule } from '@angular/cdk/text-field'
 
 @NgModule({
   declarations: [HomePageComponent, MiCatalogoPageComponent, AgregarLibroComponent],
