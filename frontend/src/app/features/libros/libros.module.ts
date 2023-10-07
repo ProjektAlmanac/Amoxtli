01253b69b395f81fa40393d360999ab71b09d7b8
Gisellrom <romero.giselln@gmail.com>
2023-10-06 18:43:48 -0600

    muestra un libro de los libros disponibles

--- frontend/src/app/features/libros/libros.module.ts
+++ frontend/src/app/features/libros/libros.module.ts
@@ -1,12 +1,37 @@
 import { NgModule } from '@angular/core'
-import { CommonModule } from '@angular/common'
+import { CommonModule, NgIf } from '@angular/common'
 import { RouterModule } from '@angular/router'
 import { MostrarLibrosComponent } from './pages/mostrar-libros/mostrar-libros.component'
 import { DetallesLibroComponent } from './components/detalles-libro/detalles-libro.component'
 
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatButtonModule } from '@angular/material/button'
+import { MatCardModule } from '@angular/material/card'
+import { MatIconModule } from '@angular/material/icon'
+import { MatInputModule } from '@angular/material/input'
+import { MatListModule } from '@angular/material/list'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { FormsModule } from '@angular/forms'
+import { SharedModule } from 'src/app/shared/shared.module'
+
 @NgModule({
   declarations: [MostrarLibrosComponent, DetallesLibroComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    FormsModule,
+    NgIf,
+    SharedModule,
+  ],
   exports: [MostrarLibrosComponent, DetallesLibroComponent],
 })
 export class librosModule {}
