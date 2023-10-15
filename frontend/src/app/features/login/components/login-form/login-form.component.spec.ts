71d5aaf5c12cba68fac7ebde3e6896ca20e85fc8
Miguel Guzman <miguel.gm98@gmail.com>
2023-10-15 15:19:50 -0600

    Corregir pruebas

--- frontend/src/app/features/login/components/login-form/login-form.component.spec.ts
+++ frontend/src/app/features/login/components/login-form/login-form.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { LoginFormComponent } from './login-form.component'
+import { LoginModule } from '../../login.module'
+import { HttpClientTestingModule } from '@angular/common/http/testing'
+import { NoopAnimationsModule } from '@angular/platform-browser/animations'
+import { RouterTestingModule } from '@angular/router/testing'
 
 describe('LoginFormComponent', () => {
   let component: LoginFormComponent
@@ -9,6 +13,7 @@ describe('LoginFormComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [LoginFormComponent],
+      imports: [LoginModule, NoopAnimationsModule, RouterTestingModule, HttpClientTestingModule],
     })
     fixture = TestBed.createComponent(LoginFormComponent)
     component = fixture.componentInstance
--- frontend/src/app/features/login/pages/login-page/login-page.component.spec.ts
+++ frontend/src/app/features/login/pages/login-page/login-page.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { LoginPageComponent } from './login-page.component'
+import { LoginModule } from '../../login.module'
+import { RouterTestingModule } from '@angular/router/testing'
+import { NoopAnimationsModule } from '@angular/platform-browser/animations'
+import { HttpClientTestingModule } from '@angular/common/http/testing'
 
 describe('LoginPageComponent', () => {
   let component: LoginPageComponent
@@ -9,6 +13,7 @@ describe('LoginPageComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [LoginPageComponent],
+      imports: [LoginModule, NoopAnimationsModule, RouterTestingModule, HttpClientTestingModule],
     })
     fixture = TestBed.createComponent(LoginPageComponent)
     component = fixture.componentInstance
