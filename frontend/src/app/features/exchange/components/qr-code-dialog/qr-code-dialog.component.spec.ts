3b6f7c10deffbf57fb3147ce5233da9eb659cb29
Antar Espadas <antar.espadas@hotmail.com>
2023-10-19 15:11:28 -0600

    Corrige pruebas

--- frontend/src/app/features/exchange/components/qr-code-dialog/qr-code-dialog.component.spec.ts
+++ frontend/src/app/features/exchange/components/qr-code-dialog/qr-code-dialog.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { QrCodeDialogComponent } from './qr-code-dialog.component'
+import { HttpClientTestingModule } from '@angular/common/http/testing'
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog'
+import { ExchangeModule } from '../../exchange.module'
 
 describe('QrCodeDialogComponent', () => {
   let component: QrCodeDialogComponent
@@ -9,6 +12,11 @@ describe('QrCodeDialogComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [QrCodeDialogComponent],
+      imports: [HttpClientTestingModule, ExchangeModule],
+      providers: [
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: { exchangeId: 1, userId: 1 } },
+      ],
     })
     fixture = TestBed.createComponent(QrCodeDialogComponent)
     component = fixture.componentInstance
--- frontend/src/app/features/exchange/components/qr-code-scanner-dialog/qr-code-scanner-dialog.component.spec.ts
+++ frontend/src/app/features/exchange/components/qr-code-scanner-dialog/qr-code-scanner-dialog.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { QrCodeScannerDialogComponent } from './qr-code-scanner-dialog.component'
+import { HttpClientTestingModule } from '@angular/common/http/testing'
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog'
+import { ExchangeModule } from '../../exchange.module'
 
 describe('QrCodeScannerDialogComponent', () => {
   let component: QrCodeScannerDialogComponent
@@ -9,6 +12,11 @@ describe('QrCodeScannerDialogComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [QrCodeScannerDialogComponent],
+      imports: [HttpClientTestingModule, ExchangeModule],
+      providers: [
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: { exchangeId: 1, code: 'code' } },
+      ],
     })
     fixture = TestBed.createComponent(QrCodeScannerDialogComponent)
     component = fixture.componentInstance
