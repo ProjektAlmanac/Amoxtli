1cd6a3dfe0b421c5c8ba97ec34c19a0454686f50
Antar Espadas <antar.espadas@hotmail.com>
2023-10-18 21:56:23 -0600

    Corrige pruebas

--- frontend/src/app/features/exchange/components/book-dialog/book-dialog.component.spec.ts
+++ frontend/src/app/features/exchange/components/book-dialog/book-dialog.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { BookDialogComponent } from './book-dialog.component'
+import { HttpClientTestingModule } from '@angular/common/http/testing'
+import { ExchangeModule } from '../../exchange.module'
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog'
 
 describe('BookDialogComponent', () => {
   let component: BookDialogComponent
@@ -9,6 +12,11 @@ describe('BookDialogComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [BookDialogComponent],
+      imports: [HttpClientTestingModule, ExchangeModule],
+      providers: [
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: { isbn: '' } },
+      ],
     })
     fixture = TestBed.createComponent(BookDialogComponent)
     component = fixture.componentInstance
