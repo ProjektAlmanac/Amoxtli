dcdc5e90a4bea010e48f7dcba5751aca1d602ca4
Antar Espadas <antar.espadas@hotmail.com>
2023-10-19 20:16:37 -0600

    Corrige y simplifica cosas

--- frontend/src/app/features/catalogo-usuario/pages/home-page/home-page.component.ts
+++ frontend/src/app/features/catalogo-usuario/pages/home-page/home-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 import { FormBuilder, FormGroup, Validators } from '@angular/forms'
-import { DefaultService, PaginaLibros, InfoBasicaLibro } from 'src/generated/openapi'
+import { Router } from '@angular/router'
+import { DefaultService, PaginaLibros } from 'src/generated/openapi'
 
 @Component({
   selector: 'app-home-page',
@@ -8,20 +9,15 @@ import { DefaultService, PaginaLibros, InfoBasicaLibro } from 'src/generated/ope
   styleUrls: ['./home-page.component.sass'],
 })
 export class HomePageComponent implements OnInit {
-  public libros!: PaginaLibros
-  public libroscopy!: InfoBasicaLibro[]
+  public paginaLibros: PaginaLibros | undefined
   public mostrarSpinner = false
-  public isbnFilter = ''
-  form: FormGroup
-  public ant = 0
-  public sig = 0
-  public res = 0
-
   public currentPage = 1
+  form: FormGroup
 
   constructor(
     private serviceApi: DefaultService,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private router: Router
   ) {
     this.form = this.fb.group({
       isbn: ['', [Validators.minLength(10), Validators.maxLength(13)]],
@@ -30,56 +26,46 @@ export class HomePageComponent implements OnInit {
 
   ngOnInit(): void {
     this.mostrarSpinner = true
-    this.recuperaLibros()
+    this.recuperaLibros(this.currentPage)
   }
 
-  recuperaLibros() {
-    this.serviceApi.getLibros(this.currentPage).subscribe(resp => {
-      this.libros = resp
-      this.libroscopy = resp.libros
-      this.ant = resp.pagAnterior
-      this.sig = resp.pagSiguiente
-      this.res = resp.resultados
+  recuperaLibros(page: number) {
+    this.serviceApi.getLibros(page).subscribe(paginaLibros => {
+      this.paginaLibros = paginaLibros
+      this.currentPage = page
       this.mostrarSpinner = false
     })
   }
+
   onSubmit() {
-    this.mostrarSpinner = true
     if (!this.form.valid) {
-      this.mostrarSpinner = false
       this.form.markAllAsTouched()
       return
     }
 
     const isbn = this.form.get('isbn')?.value
-    if (isbn !== null && isbn !== undefined) {
-      this.filtrarLibros(isbn) // Filtrar los libros basados en el ISBN
-    } else {
-      // En caso de que no se haya ingresado un ISBN, muestra el mensaje de "No se encontraron libros"
-      this.libros.libros = this.libroscopy
-      this.mostrarSpinner = false
-    }
-  }
+    if (isbn === null || isbn === undefined) return
 
-  filtrarLibros(isbn: string) {
-    if (isbn) {
-      // Filter the libros array based on the entered ISBN
-      this.mostrarSpinner = false
-      this.libros.libros = this.libros.libros.filter(libro => libro.isbn.includes(isbn))
-    } else {
-      this.mostrarSpinner = false
-      this.libros.libros = this.libroscopy
-      this.form.reset()
-    }
+    this.mostrarSpinner = true
+
+    this.serviceApi.getDetallesLibro(isbn).subscribe({
+      next: () => {
+        this.router.navigate(['/libro'], { queryParams: { isbn } })
+      },
+      error: () => {
+        // Mostrar el mismo mensaje de error que cuando no se encuentran libros
+        this.paginaLibros = undefined
+      },
+    })
   }
 
   irAnterior() {
-    this.currentPage = this.ant
-    this.recuperaLibros()
+    if (this.paginaLibros === undefined) return
+    this.recuperaLibros(this.paginaLibros.pagAnterior)
   }
 
   irSiguiente() {
-    this.currentPage = this.sig
-    this.recuperaLibros()
+    if (this.paginaLibros === undefined) return
+    this.recuperaLibros(this.paginaLibros.pagSiguiente)
   }
 }
