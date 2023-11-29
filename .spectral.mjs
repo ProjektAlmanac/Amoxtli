7a005b585f557ef0459055e78efbab9b355a2064
Antar Espadas <espadas.antar@gmail.com>
2023-11-28 22:19:57 -0600

    Prueba de push para stoplight

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIyNzkwMg/spectral.js?branch_slug=master&token=a8517f95-7e95-41e8-ae73-fd0d47f874cb";
+export default { extends: ruleset };
